63b18f76aa647295c2ad95132cbaa0782a38eb48
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2026-04-23 13:07:10 +0800

    y

--- src/plugin/echo.ts
+++ src/plugin/echo.ts
@@ -13,9 +13,9 @@ const echo = new Echo({
   broadcaster: "reverb",
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: 443,
-  wssPort: 443, // Idagdag ito
-  forceTLS: true,
+  wsPort: 8080,
+  wssPort: 8080, // Idagdag ito
+  forceTLS: false,
   enabledTransports: ["wss", "ws"],
 })
 
